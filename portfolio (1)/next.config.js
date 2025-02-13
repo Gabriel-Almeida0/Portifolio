98f1104fcde2477bfa223339fe98ca39476c0849
Gabriel.Almeida-10 <105819653+Gabriel-Almeida0@users.noreply.github.com>
2025-02-12 23:49:04 -0300

    Delete portfolio (1) directory

--- portfolio (1)/next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    basePath: '/portifolio',
-    assetPrefix: '/portifolio/',
-    output: 'export',
-    images: {
-      unoptimized: true, // necessário para não gerar erros com imagens
-    },
-    trailingSlash: true, // garante que as rotas sejam geradas com barra no final
-  };
